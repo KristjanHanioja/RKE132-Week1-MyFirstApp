23e57de5759fbd56068e047cb0548bdd33366fa3
KristjanH <kristjan.hanioja@tktk.ee>
2024-04-11 12:19:25 +0300

    create new app

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// This is a comment to my code 
+
+
+// Rakendus küsib kasutajalt sisestada tema nime
+// Rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Enter your name: "); // output
+// string - sõne
+string userName = Console.ReadLine(); // input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); // output
+
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !"); // output
